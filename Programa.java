86fbeb972d5cdbb193710a9fa2fef81ae32c9a75
Adao Aurelio <adaoaurelio@gmail.com>
2021-02-24 20:53:44 -0300

    Aula1

--- Programa.java
+++ Programa.java
@@ -2,12 +2,21 @@ public class Programa{
     public static void maio(String args[]){
         int inteiro;
         String nome = "Adão";
+        //nome = "Adão"
+        float flutuante = 3.2f;
+        float Flutuante2 = (float) 3.2;
+        Float vetor[];
+
+        vetor = new float[];
+
+        nome = "Adão Aurélio";
 
         inteiro = 30;
 
-        nome = "Adão Aurélio"
-
         System.out.println("inteiro: " + inteiro); //print
+
+        /* If, Else, While e Do While é igual C (A maioria das operações são assim só muda a sintaxe)
+        *\
 
     }
 }
