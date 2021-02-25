b6c123110e933fab1723fd6d4c5c144d4ee70418
Adao Aurelio <adaoaurelio@gmail.com>
2021-02-24 23:11:14 -0300

    Done

---
+++ Lista_1/Exercicio4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercicio4{
+    public static void main(String[] args) {
+       int res1 = 6;
+       int res2 = 2;
+       int i;
+
+       for(i = 1; i < 11 ; i++)
+        System.out.println(i + " x 6 = " + (i*res1) +"     "+ i + " x 2 = " + (i*res2));
+    }
+}
