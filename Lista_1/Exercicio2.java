19fee260b1ea81a45338d3835034b6648292eae8
Adao Aurelio <adaoaurelio@gmail.com>
2021-02-24 22:25:26 -0300

    Done

---
+++ Lista_1/Exercicio2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio2{
+    public static void main(String[] args) {
+        int inteiro1, inteiro2;
+        Double double1, double2;
+
+        inteiro1 = 2;
+        inteiro2 = 80;
+
+        double1 = 2.3;
+        double2 = 70.5;
+
+        System.out.println(inteiro1+inteiro2);
+        System.out.println(double1+double2);
+
+    }
+}
