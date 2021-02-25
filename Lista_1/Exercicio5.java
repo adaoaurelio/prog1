1e1399dd803350e546ab9b9e31a02b693cbafda5
Adao Aurelio <adaoaurelio@gmail.com>
2021-02-25 08:33:47 -0300

    Change

---
+++ Lista_1/Exercicio5.java
@@ -0,0 +1,19 @@
+package Lista_1;
+
+import java.util.Vector;
+
+public class Exercicio5 {
+    
+    public static void main(String[] args) {
+        Vector repeat = "atrapalham";
+        int i;
+
+        System.out.println("1 Java atrapalha muita gente");
+
+        for(i = 1; i < 101; i++){
+            System.out.println(i + " Java "+ repeat +" muita gente");
+        }
+    }
+}
+
+//TESTE
