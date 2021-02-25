c7daaeee15315d8d720b7ca97638bbdfb6f6aa21
Adao Aurelio <adaoaurelio@gmail.com>
2021-02-24 22:59:12 -0300

    Done

---
+++ Lista_1/Exercicio3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Exercicio3{
+    public static void main(String[] args) {
+       int resultado = 5;
+       int i;
+
+       for(i = 1; i < 11 ; i++)
+        System.out.println(i + " x 5 = " + (i*resultado));
+    }
+}
