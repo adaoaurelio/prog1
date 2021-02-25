d6158b8dd7dc6c31bcc7612185775d6f811994ad
Adao Aurelio <adaoaurelio@gmail.com>
2021-02-24 21:58:50 -0300

    commit

--- Lista_1/Exercicio1.java
+++ Lista_1/Exercicio1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Exercicio1{
-    public static void public static void main(String[] args) {
+    public static void main(String[] args) {
         
     }
 }
