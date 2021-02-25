4f68bb99c162928e0c04db6047327a8d799aed48
Adao Aurelio <adaoaurelio@gmail.com>
2021-02-24 22:09:00 -0300

    Done

--- Lista_1/Exercicio1.java
+++ Lista_1/Exercicio1.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class Exercicio1{
     public static void main(String[] args) {
-        
+        System.out.println("Programação I");
+        System.out.println("- Professor: Fernando Bevilacqua");
+        System.out.println("- Aluno: Adão Aurélio");
+        System.out.println("- Objetivo:");
+        System.out.println("         O objetivo dessa disciplina é compreender os conceitos fundamentais");
+        System.out.println("         do paradigma de programação orientada a objetos e aplicá-los no");
+        System.out.println("         desenvolvimento de soluções de software.");
+        System.out.println("- Turno: Noturno");
     }
 }
