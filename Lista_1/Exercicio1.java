13c3dccf522ce8571401895d71a8f540a4124fd7
Adao Aurelio <adaoaurelio@gmail.com>
2021-02-24 22:15:21 -0300

    Done

--- Lista_1/Exercicio1.java
+++ Lista_1/Exercicio1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Exercicio1{
     public static void main(String[] args) {
         System.out.println("Programação I");
-        System.out.println("- Professor: Fernando Bevilacqua");
+        System.out.println("- Professores: Andrei Braga | Geomar Andre Schreiner");
         System.out.println("- Aluno: Adão Aurélio");
         System.out.println("- Objetivo:");
         System.out.println("         O objetivo dessa disciplina é compreender os conceitos fundamentais");
