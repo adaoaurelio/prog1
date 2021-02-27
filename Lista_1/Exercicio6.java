fd45660b0bb504451738b8df345d0aafd8b75be6
Adao Aurelio <adaoaurelio@gmail.com>
2021-02-27 14:13:43 -0300

    Done

---
+++ Lista_1/Exercicio6.java
@@ -0,0 +1,10 @@
+package Lista_1;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        int nasc = 1993, presente = 2021;
+
+        System.out.println("Idade" + presente-nasc);
+    }
+    
+}
---
+++ Lista_1/Exercicio7.java
@@ -0,0 +1,23 @@
+package Lista_1;
+
+public class Exercicio7 {
+    public static void main(String[] args) {
+        int  i, j, count=0;
+
+        for(i = 0; i <= 50; i++) {            
+
+            for (j = 1; j <= i; j++) {
+                if (i % j == 0){
+                    count++;
+                }
+            }
+     
+            if (count == 2){
+                    System.out.println("O numero "+ i +" eh primo");   
+            }
+            
+                count=0;
+        }
+    }
+}
+
