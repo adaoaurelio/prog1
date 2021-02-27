69946c95041716b701d6adcdc7c780a6bbee4e5d
Adao Aurelio <adaoaurelio@gmail.com>
2021-02-27 10:12:26 -0300

    done

--- Lista_1/Exercicio5.java
+++ Lista_1/Exercicio5.java
@@ -5,13 +5,13 @@ import java.util.Vector;
 public class Exercicio5 {
     
     public static void main(String[] args) {
-        Vector repeat = "atrapalham";
+        String repeat = " atrapalham ";
         int i;
 
         System.out.println("1 Java atrapalha muita gente");
 
-        for(i = 1; i < 101; i++){
-            System.out.println(i + " Java "+ repeat +" muita gente");
+        for(i = 2; i < 101; i++){
+            System.out.println(i + " Java "+ repeat.repeat(i) +" muita gente");
         }
     }
 }
