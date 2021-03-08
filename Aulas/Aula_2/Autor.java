4cc1b4a0f00aed2130df7603f1bc8e0018612ce4
Adao Aurelio <adaoaurelio@gmail.com>
2021-03-07 21:02:59 -0300

    changes

---
+++ Aulas/Aula_2/Autor.java
@@ -0,0 +1,10 @@
+class Autor {
+    String nome;
+    int id;
+
+    void carregaDoTeclado(String nome, int id){
+        this.nome = nome;
+        this.id = id;
+
+    }
+}
---
+++ Aulas/Aula_2/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Autor autor = new Autor();
+        
+        Scanner sc = new Scanner(System.in);
+
+        String autor.nome = sc.nextLine();
+        int autor.id = sc.nextInt();
+        
+        Autor.carregaDoTeclado(autor.id, autor.nome);
+
+    }
+    
+}
