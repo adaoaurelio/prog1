390383a03e44dad2e383601a9ce7c2cf20aaaaba
Adao Aurelio <adaoaurelio@gmail.com>
2021-03-24 10:14:44 -0300

    teste

--- Aulas/Aula_2/Autor.java
+++ Aulas/Aula_2/Autor.java
@@ -2,9 +2,10 @@ class Autor {
     String nome;
     int id;
 
-    void carregaDoTeclado(String nome, int id){
+    String carregaDoTeclado(String nome, int id){
         this.nome = nome;
         this.id = id;
 
     }
+    
 }
