1855ef49d8a4f6b5d692f8237294f7f1ef2313d8
Adao Aurelio <adaoaurelio@gmail.com>
2021-03-25 10:16:42 -0300

    git manhã

---
+++ Lista_3/Agenda.java
@@ -0,0 +1,11 @@
+package Lista_3;
+
+public class Agenda {
+   public void armazenaPessoa(String nome, int idade, float altura);
+   public void removePessoa(String nome);
+   public int buscaPessoa(String nome); // informa em que posição da agenda está a pessoa
+   public void imprimeAgenda(); // imprime os dados de todas as pessoas da agenda
+   public void imprimePessoa(int index); // imprime os dados da pessoa que está na posição
+    “index” da agenda.
+
+}
